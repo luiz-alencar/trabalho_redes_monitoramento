9d50119a2991325f89739230b3a6713052dd1b91
Luiz Gustavo Alves Alencar <114269593+luiz-alencar@users.noreply.github.com>
2023-10-27 22:27:18 -0300

    add vagrant

---
+++ vagrant/route2.sh
@@ -0,0 +1,8 @@
+sudo sysctl -w net.ipv4.ip_forward=1
+apt update
+apt install net-tools
+ifconfig enp0s3 down
+route del default gw 10.0.2.2
+
+sudo route add -net 192.168.0.0/24 gw 192.168.0.254
+sudo route add -net 10.0.0.0/24 gw 10.0.0.254
