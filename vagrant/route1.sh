c3cf0c2802ce1d74269881b3bef303db31e29255
Luiz Gustavo Alves Alencar <114269593+luiz-alencar@users.noreply.github.com>
2023-11-05 17:13:24 -0300

    Ajustes finais e configurando tcpdump

--- vagrant/route1.sh
+++ vagrant/route1.sh
@@ -1,5 +1,5 @@
 apt update
 apt install net-tools
-ifconfig enp0s3 donw
+#ifconfig enp0s3 donw
 route del default gw 10.0.2.2
-route add default gw 192.168.0.254
+route add default gw 192.168.50.254
--- vagrant/route2.sh
+++ vagrant/route2.sh
@@ -1,8 +1,10 @@
 sudo sysctl -w net.ipv4.ip_forward=1
 apt update
 apt install net-tools
-ifconfig enp0s3 down
+sudo apt install tcpdump
+
+#ifconfig enp0s3 down
 route del default gw 10.0.2.2
 
-sudo route add -net 192.168.0.0/24 gw 192.168.0.254
+sudo route add -net 192.168.0.0/24 gw 192.168.50.254
 sudo route add -net 10.0.0.0/24 gw 10.0.0.254
